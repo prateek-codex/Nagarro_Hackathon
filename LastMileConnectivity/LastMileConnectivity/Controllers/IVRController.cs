8570a07d9979d3e137d8f5fbb8e743d2144ae5c5
Prateek Varshney <prateekvarshney@nagarro.com>
2017-03-19 13:13:42 +0530

    IVR integration using Twilo

---
+++ LastMileConnectivity/LastMileConnectivity/Controllers/IVRController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LastMileConnectivity.Controllers
+{
+    public class IVRController : ApiController
+    {
+        public void Get()
+        {
+            new IVRIntegration().Connect();
+        }
+    }
+}
